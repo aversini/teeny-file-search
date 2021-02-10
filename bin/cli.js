5580e81ef8fa486e29200a2d41f5658805585f2e
Arno Versini <arno.versini@wellsfargo.com>
2021-02-09 22:00:03 -0800

    feat: migrating from commander to meow for a better CLI experience

--- bin/cli.js
+++ bin/cli.js
@@ -1,99 +1,181 @@
 #!/usr/bin/env node
 
 const { blue, grey } = require("kleur");
-const path = require("path");
 const fs = require("fs-extra");
-const commander = require("commander");
+const meow = require("meow");
 const { displayErrorMessages, shallowMerge } = require("teeny-js-utilities");
 const PrettyError = require("pretty-error");
+const TeenyLogger = require("teeny-logger");
+const logger = new TeenyLogger({
+  boring: process.env.NODE_ENV === "test",
+});
 
 const defaults = require("../src/defaults");
 const { Search } = require("../src/search");
-const pkg = require(path.join(__dirname, "../package.json"));
 
-const program = new commander.Command();
 const pe = new PrettyError();
 // Automatically prettifying all exceptions that are logged
 pe.start();
 
-const optionParseType = (value) => {
-  if (value !== "f" && value !== "d") {
-    throw new commander.InvalidOptionArgumentError(
-      `Valid options are "f" or "d".`
-    );
-  }
-  return value;
-};
-
-const optionParseGrep = (value) => {
-  if (value !== "" && program.opts().type === "d") {
-    throw new commander.InvalidOptionArgumentError(
-      `Options "grep" and "type" = "d" are incompatible.`
-    );
-  }
-  return value;
-};
-
-program
-  .version(pkg.version, "-v, --version", "Output the current version")
-  .arguments("[path]")
-  .option(
-    "-p, --pattern <string>",
-    "A regular expression to match file or folder names",
-    null
-  )
-  .option(
-    "-t, --type <string>",
-    "Search for files (f) or directories (d)",
-    optionParseType
-  )
-  .option("--short", "Short listing format (equivalent to ls)", false)
-  .option("--dot", "Show hidden files and directories", false)
-  .option("-b, --boring", "Do not use color output", false)
-  .option("-s, --stats", "Display some statistics", false)
-  .option("-i, --ignore-case", "Ignore case when searching", false)
-  .option(
-    "-c, --command <cmd>",
-    "Command to execute over each node (ex: chmod +x)"
-  )
-  .option(
-    "-g, --grep <pattern>",
-    "A regular expression to match the content of the files found",
-    optionParseGrep
-  )
-  .helpOption("-h, --help", "Display help instructions");
-
-program.addHelpText(
-  "after",
-  `\nPath: the path where to search for files or directories (default: current folder)
-
-Examples:
-  ${grey('## Find files with the extension ".jsx" in the "src" folder')}
-  ${blue('> teeny-file-search --type f --pattern ".jsx$" src')}
-
-  ${grey(
-    '## Change the permissions to executable for all the files with extension ".sh" found under the "bin" folder'
-  )}
-  ${blue('> teeny-file-search --type f --pattern=".sh$" --command "chmod +x"')}
-
-  ${grey(
-    'Search in all the markdown files under the `src` folder for the keywords "Table of Content"'
-  )}
-  ${blue(
-    '> teeny-file-search -type f --pattern ".md$" --grep "Table of Content"'
-  )}
+const cli = meow(
   `
+  Usage:
+    $ teeny-file-search [options] [path]
+
+  Options:
+    ${blue("-b, --boring")}            Do not use color output ${grey(
+    "(default: false)"
+  )}
+    ${blue(
+      "-c, --command <cmd>"
+    )}     Command to execute over each node (ex: chmod +x)
+    ${blue("    --dot")}               Show hidden files and directories ${grey(
+    "(default: false)"
+  )}
+    ${blue(
+      "-g, --grep <pattern>"
+    )}    A regular expression to match the content of the files found
+    ${blue("-h, --help")}              Display help instructions
+    ${blue("-i, --ignore-case")}       Ignore case when searching ${grey(
+    "(default: false)"
+  )}
+    ${blue(
+      "-p, --pattern <string>"
+    )}  A regular expression to match file or folder names ${grey(
+    "(default: null)"
+  )}
+    ${blue("-s, --stats")}             Display some statistics ${grey(
+    "(default: false)"
+  )}
+    ${blue(
+      "    --short"
+    )}             Short listing format (equivalent to ls) ${grey(
+    "(default: false)"
+  )}
+    ${blue("-t, --type <string>")}     Search for files (f) or directories (d)
+    ${blue("-v, --version")}           Output the current version
+
+  ${blue("Path:")} the path where to search for files or directories ${grey(
+    "(default: current folder)"
+  )}
+
+  Examples:
+    ${grey('## Find files with the extension ".jsx" in the "src" folder')}
+    ${blue('> teeny-file-search --type f --pattern ".jsx$" src')}
+
+    ${grey(
+      '## Change the permissions to executable for all the files with extension ".sh" found under the "bin" folder'
+    )}
+    ${blue(
+      '> teeny-file-search --type f --pattern=".sh$" --command "chmod +x"'
+    )}
+
+    ${grey(
+      'Search in all the markdown files under the `src` folder for the keywords "Table of Content"'
+    )}
+    ${blue(
+      '> teeny-file-search --type f --pattern ".md$" --grep "Table of Content"'
+    )}
+`,
+  {
+    allowUnknownFlags: false,
+    autoHelp: false,
+    autoVersion: false,
+    description: false,
+    flags: {
+      boring: {
+        alias: "b",
+        default: false,
+        type: "boolean",
+      },
+      command: {
+        alias: "c",
+        type: "string",
+      },
+      dot: {
+        default: false,
+        type: "boolean",
+      },
+      grep: {
+        alias: "g",
+        type: "string",
+      },
+      help: {
+        alias: "h",
+        type: "boolean",
+      },
+      ignoreCase: {
+        alias: "i",
+        default: false,
+        type: "boolean",
+      },
+      pattern: {
+        alias: "p",
+        type: "string",
+      },
+      short: {
+        default: false,
+        type: "boolean",
+      },
+      stats: {
+        alias: "s",
+        default: false,
+        type: "boolean",
+      },
+      type: {
+        alias: "t",
+        type: "string",
+      },
+      version: {
+        alias: "v",
+        type: "boolean",
+      },
+    },
+  }
 );
 
-program.configureHelp({
-  sortOptions: true,
-});
+const { command, grep, help, type, version } = cli.flags;
 
-program.parse(process.argv);
-const customCfg = program.opts();
+if (help) {
+  cli.showHelp();
+  process.exit(0);
+}
 
-if (program.args.length) {
-  const customPath = program.args[0];
+if (version) {
+  cli.showVersion();
+  process.exit(0);
+}
+
+if (typeof type === "string" && type !== "d" && type !== "f") {
+  logger.error(
+    `Error: option '-t, --type <string>' argument '${type}' is invalid. Valid options are "f" or "d".`
+  );
+  process.exit(1);
+}
+
+if (typeof command === "string" && !command) {
+  logger.error(
+    `Error: option '-c, --command <cmd>' argument '${command}' is invalid.`
+  );
+  process.exit(1);
+}
+
+if (typeof grep === "string" && !grep) {
+  logger.error(
+    `Error: option '-g, --grep <pattern>' argument '${grep}' is invalid.`
+  );
+  process.exit(1);
+}
+
+if (typeof grep === "string" && typeof type === "string" && type === "d") {
+  logger.error(`Error: options "grep" and "type" = "d" are incompatible.`);
+  process.exit(1);
+}
+
+const customCfg = cli.flags;
+
+if (cli.input.length) {
+  const customPath = cli.input[0];
   if (fs.pathExistsSync(customPath)) {
     customCfg.path = customPath;
   } else {
