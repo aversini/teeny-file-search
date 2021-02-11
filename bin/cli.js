8fd65219eff995f876d76ab6a4f873da7e850f00
Arno Versini <arno.versini@wellsfargo.com>
2021-02-10 19:57:53 -0800

    chore: using meowOptionsHelper() to format meow help automatically

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
-const { blue, grey } = require("kleur");
 const fs = require("fs-extra");
 const meow = require("meow");
-const { displayErrorMessages, shallowMerge } = require("teeny-js-utilities");
+const {
+  displayErrorMessages,
+  meowOptionsHelper,
+  shallowMerge,
+} = require("teeny-js-utilities");
 const PrettyError = require("pretty-error");
 const TeenyLogger = require("teeny-logger");
 const logger = new TeenyLogger({
@@ -17,122 +20,97 @@ const pe = new PrettyError();
 // Automatically prettifying all exceptions that are logged
 pe.start();
 
-const cli = meow(
-  `
-  Usage:
-    $ teeny-file-search [options] [path]
-
-  Options:
-    ${blue("-b, --boring")}            Do not use color output ${grey(
-    "(default: false)"
-  )}
-    ${blue(
-      "-c, --command <cmd>"
-    )}     Command to execute over each node (ex: chmod +x)
-    ${blue("    --dot")}               Show hidden files and directories ${grey(
-    "(default: false)"
-  )}
-    ${blue(
-      "-g, --grep <pattern>"
-    )}    A regular expression to match the content of the files found
-    ${blue("-h, --help")}              Display help instructions
-    ${blue("-i, --ignore-case")}       Ignore case when searching ${grey(
-    "(default: false)"
-  )}
-    ${blue(
-      "-p, --pattern <string>"
-    )}  A regular expression to match file or folder names ${grey(
-    "(default: null)"
-  )}
-    ${blue("-s, --stats")}             Display some statistics ${grey(
-    "(default: false)"
-  )}
-    ${blue(
-      "    --short"
-    )}             Short listing format (equivalent to ls) ${grey(
-    "(default: false)"
-  )}
-    ${blue("-t, --type <string>")}     Search for files (f) or directories (d)
-    ${blue("-v, --version")}           Output the current version
-
-  ${blue("Path:")} the path where to search for files or directories ${grey(
-    "(default: current folder)"
-  )}
-
-  Examples:
-    ${grey('## Find files with the extension ".jsx" in the "src" folder')}
-    ${blue('> teeny-file-search --type f --pattern ".jsx$" src')}
-
-    ${grey(
-      '## Change the permissions to executable for all the files with extension ".sh" found under the "bin" folder'
-    )}
-    ${blue(
-      '> teeny-file-search --type f --pattern=".sh$" --command "chmod +x"'
-    )}
-
-    ${grey(
-      'Search in all the markdown files under the `src` folder for the keywords "Table of Content"'
-    )}
-    ${blue(
-      '> teeny-file-search --type f --pattern ".md$" --grep "Table of Content"'
-    )}
-`,
-  {
-    allowUnknownFlags: false,
-    autoHelp: false,
-    autoVersion: false,
-    description: false,
-    flags: {
-      boring: {
-        alias: "b",
-        default: false,
-        type: "boolean",
-      },
-      command: {
-        alias: "c",
-        type: "string",
-      },
-      dot: {
-        default: false,
-        type: "boolean",
-      },
-      grep: {
-        alias: "g",
-        type: "string",
-      },
-      help: {
-        alias: "h",
-        type: "boolean",
-      },
-      ignoreCase: {
-        alias: "i",
-        default: false,
-        type: "boolean",
-      },
-      pattern: {
-        alias: "p",
-        type: "string",
-      },
-      short: {
-        default: false,
-        type: "boolean",
-      },
-      stats: {
-        alias: "s",
-        default: false,
-        type: "boolean",
-      },
-      type: {
-        alias: "t",
-        type: "string",
-      },
-      version: {
-        alias: "v",
-        type: "boolean",
-      },
+const { helpText, options } = meowOptionsHelper({
+  examples: [
+    {
+      command:
+        'teeny-file-search --type f --pattern=".sh$" --command "chmod +x"',
+      comment: '## Find files with the extension ".jsx" in the "src" folder',
     },
-  }
-);
+    {
+      command:
+        'teeny-file-search --type f --pattern=".sh$" --command "chmod +x"',
+      comment:
+        '## Change the permissions to executable for all the files with extension ".sh" found under the "bin" folder',
+    },
+    {
+      command:
+        'teeny-file-search --type f --pattern ".md$" --grep "Table of Content"',
+      comment:
+        '## Search in all the markdown files under the `src` folder for the keywords "Table of Content"',
+    },
+  ],
+  flags: {
+    boring: {
+      alias: "b",
+      default: false,
+      description: "Do not use color output",
+      type: "boolean",
+    },
+    command: {
+      alias: "c",
+      description: "Command to execute over each node (ex: chmod +x)",
+      type: "string",
+    },
+    dot: {
+      default: false,
+      description: "Show hidden files and directories",
+      type: "boolean",
+    },
+    grep: {
+      alias: "g",
+      description:
+        "A regular expression to match the content of the files found",
+      type: "string",
+    },
+    help: {
+      alias: "h",
+      description: "Display help instructions",
+      type: "boolean",
+    },
+    ignoreCase: {
+      alias: "i",
+      default: false,
+      description: "Ignore case when searching",
+      type: "boolean",
+    },
+    pattern: {
+      alias: "p",
+      description: "A regular expression to match file or folder names",
+      type: "string",
+    },
+    short: {
+      default: false,
+      description: "Short listing format (equivalent to ls)",
+      type: "boolean",
+    },
+    stats: {
+      alias: "s",
+      default: false,
+      description: "Display some statistics",
+      type: "boolean",
+    },
+    type: {
+      alias: "t",
+      description: "Search for files (f) or directories (d)",
+      type: "string",
+    },
+    version: {
+      alias: "v",
+      description: "Output the current version",
+      type: "boolean",
+    },
+  },
+  parameters: {
+    path: {
+      default: "current folder",
+      description: "the path where to search for files or directories",
+    },
+  },
+  usage: "teeny-file-search [options] [path]",
+});
+
+const cli = meow(helpText, options);
 
 const { command, grep, help, type, version } = cli.flags;
 
